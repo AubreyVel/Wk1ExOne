c0f06a791999e6e49a6142d905a3d0e938e8fbff
AubreyVel <agvelasco@amarillocollege.com>
2025-01-19 19:34:53 -0700

    first commit

---
+++ Wk1Ex2/Wk1Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name? "); //getting first name, put Write so it stays on the same line, savinf name to variable firstName
+            string firstName = Console.ReadLine();
+           
+            Console.Write("What is your last name? "); //getting the last name, saving to variable lastName
+            string lastName = Console.ReadLine();
+
+            Console.Write("What is your age? "); //getting age, saving to variable age. Didn't know if string was acceptable but it works so i kept it.
+            string age = Console.ReadLine();
+
+            Console.Write("What is your favorite color? ");  //getting color, saving to variable favoriteColor
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("Hello, " +firstName +" "+ lastName +"! You are " + age + " years old and your favorite color is " + favoriteColor); //putting all the variables together
+            Console.ReadLine();
+        }
+    }
+}
